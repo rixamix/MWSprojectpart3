ac95abc3a7ad577dc6285ebcad05bf5bebc1e26d
Rixa Gregory <rixa@rixamix.com>
2018-09-25 21:00:07 -0500

    fixed offline posting

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+var precache = 'precache1';
 var cacheName = 'appCache';
 //var dbPromise = idb.open('test-db', 1, function(upgradeDb){
   //if(!upgradeDb.objectStoreNames.contains('firstOS')){
@@ -5,16 +6,53 @@ var cacheName = 'appCache';
   //}
 //});
 
+var AppShell =[
+  'index.html',
+  'css/styles.css',
+  'js/dbhelper.js',
+  'js/main.js',
+  'js/restaurant_info.js'
+];
 
-
- self.addEventListener('fetch', function(event){
-  event.respondWith(
-    caches.open(cacheName).then(function(cache){
-      return cache.match(event.request).then(function(response){
-        return response || fetch(event.request).then(function(response){
-          cache.put(event.request, response.clone());
-        });
-      });
-    })
+self.addEventListener('install', (event)=>{
+  event.waitUntil(
+    caches.open(precache)
+    .then(cache => cache.addAll(AppShell))
+    .then(self.skipWaiting())
   );
 });
+
+self.addEventListener('activate',(event)=>{
+ console.log('activated serviceWorker');
+ const currentCaches = [precache,cacheName];
+ event.waitUntil(
+   caches.keys().then(cacheNames =>{
+     return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+   }).then(cachesToDelete => {
+     return Promise.all(cachesToDelete.map(cacheToDelete =>{
+       return caches.delete(cacheToDelete);
+     }));
+   }).then(()=> self.clients.claim())
+ );
+});
+
+ self.addEventListener('fetch', function(event){
+   var storageUrl = event.request.url.split(/[?#]/)[0];
+
+   if(storageUrl.startsWith(self.location.origin)){
+     event.respondWith(
+       caches.match(storageUrl).then(cachedResponse =>{
+         if (cachedResponse){
+           return cachedResponse;
+         }
+         return caches.open(cacheName).then(cache =>{
+           return fetch(event.request).then(response =>{
+             return cache.put(storageUrl, response.clone()).then(()=>{
+           return response;
+         });
+       });
+     });
+       })
+     );
+   }
+   });
